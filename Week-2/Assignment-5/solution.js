53fe514ffaff879c33909096b18527ad96fb3aa8
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-03-28 16:48:20 +0800

    Update solution for Assignment-5

--- Week-2/Assignment-5/solution.js
+++ Week-2/Assignment-5/solution.js
@@ -1,17 +1,18 @@
 function binarySearchPosition(numbers, target) {
     let l = 0, r = numbers.length;
-    while (l <= r) {
+    while (l < r) {
         let m = l + Math.floor((r - l) / 2);
         if (numbers[m] === target) {
-            return m
+            return m;
         }
-
         if (numbers[m] > target) {
-            r = m - 1;
+            r = m;
         } else {
             l = m + 1;
         }
     }
+    return l;
 }
 console.log( binarySearchPosition([1, 2, 5, 6, 7], 1) ); // should print 0 
 console.log( binarySearchPosition([1, 2, 5, 6, 7], 6) ); // should print 3
+console.log( binarySearchPosition([1, 2, 5, 6, 7], 3) ); // should print 
