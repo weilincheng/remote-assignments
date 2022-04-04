655f167dd1a684a98b6d9511541d863c3c27291a
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-04-04 22:46:54 +0800

    Update app.js to render a static html file for Assignment 1

---
+++ Week-3/Assignments/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    app.use(express.static('public/index.hmlt'));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
