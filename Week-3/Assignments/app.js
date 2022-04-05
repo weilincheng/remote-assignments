966fb5716366fb856c5cab4ec693ff26a07e952b
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-04-05 12:28:56 +0800

    Add myName routing

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 
-
+// Routing for http://localhost:3000
 app.get('/', (req, res) => {
     app.use(express.static('public'));
-    app.use(express.static('public/index.html'));
 });
 
+// Routing for http://localhost:3000/data?number=123
 app.get('/data', (req, res) => {
     const inputNumber = req.query.number;
     if (!inputNumber) {
@@ -20,6 +21,11 @@ app.get('/data', (req, res) => {
         const result = (( 1 + +inputNumber) * +inputNumber) / 2;
         res.send(`The result is: ${result}`);
     }
+});
+
+// Routing for http://localhost:3000/myName
+app.get('myName', (req, res) => {
+
 });
 
 app.listen(port, () => {
