ac9cf44084449624054369288f43629e7dedf2e5
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-04-06 08:13:28 +0800

    Add cookie support for assignment 4

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(cookieParser());
 
 // Routing for http://localhost:3000
 app.get('/', (req, res) => {
@@ -24,8 +25,26 @@ app.get('/data', (req, res) => {
 });
 
 // Routing for http://localhost:3000/myName
-app.get('myName', (req, res) => {
+app.get('/myName', (req, res) => {
+    const user_name = req.cookies['name']; 
+    if (user_name) {
+        console.log("cookie exists")
+        res.send(`${user_name}`)
+    } else {
+        console.log("cookie does not exist");
+        res.redirect('/name.html');
+    }
+});
 
+// Routing for http://localhost:3000/trackName?name=abc
+app.get('/trackName', (req, res) => {
+    const inputName = req.query.name;
+    if (!inputName) {
+        res.send('Lack of Parameter')
+    } else {
+        console.log('cookie saved');
+        res.cookie('name', inputName, {expires: new Date(Date.now() + 600000)}).send("Cookie saved");
+    }
 });
 
 app.listen(port, () => {
