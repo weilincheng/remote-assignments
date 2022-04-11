a5e57e77d32a642dabe5aa4f8e6e234807d9d185
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-04-12 07:35:24 +0800

    Add routes to check email and password

--- Week-4/Assignment-3/index.js
+++ Week-4/Assignment-3/index.js
@@ -22,10 +22,57 @@ app.use(express.static("public"));
 // Serve index.html as Login form
 app.get("/", (req, res) => {});
 
+// Serve member.html after a successful login
+app.get("/member", (req, res) => {
+    res.redirect("/member.html");
+});
+
+// Check email
+app.get("/checkEmail", (req, res) => {
+    const email = req.query.email;
+    const sql = `SELECT * FROM user WHERE email='${email}';`;
+    console.log(`Checking email: ${email}`);
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        if (result.length > 0) {
+            console.log("Email exist. Response is true");
+            res.send(true);
+        } else {
+            console.log("Email does not exist. Response is false");
+            res.send(false);
+        }
+    });
+});
+
+// Check email
+app.get("/checkPassword", (req, res) => {
+    const email = req.query.email;
+    const password = req.query.password;
+    const sql = `SELECT * FROM user WHERE email='${email}' AND password='${password}';`;
+    console.log(`Checking password: ${password} against email: ${email}`);
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        if (result.length > 0) {
+            console.log("Password matches email. Response is true");
+            res.send(true);
+        } else {
+            console.log("Password does not match email. Response is false");
+            res.send(false);
+        }
+    });
+});
+
 // Insert new user data into MySQL
-app.get("/createuser", (req, res) => {
-    const sql =
-        "INSERT INTO user(email, password) VALUES ('ttt@gmail.com', '00000');";
+app.get("/createUser", (req, res) => {
+    const email = req.query.email;
+    const password = req.query.password;
+    const sql = `INSERT INTO user(email, password) VALUES ('${email}', '${password}');`;
     db.query(sql, (err, result) => {
         if (err) {
             throw err;
