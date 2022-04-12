95f7806ef0fabf050adcfbdfcaa2b53b17d99537
weilincheng <7236196+weilincheng@users.noreply.github.com>
2022-04-12 10:23:16 +0800

    Remove comment in assignment4

--- Week-4/Assignment-4/solution.js
+++ Week-4/Assignment-4/solution.js
@@ -11,6 +11,5 @@ delayedResultPromise(4, 5, 3000).then(console.log); // 9 (4+5) will be shown in 
 async function main() {
     const result = await delayedResultPromise(4, 5, 3000);
     console.log(result);
-    // your code here, you should call delayedResultPromise here and get the result using async/await.
 }
 main(); // result will be shown in the console after <delayTime> seconds
